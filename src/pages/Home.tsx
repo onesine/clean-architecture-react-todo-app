5ae02d1343f16ea96ae8d641a76e45e83a18dd93
onesine <philemonlewhe@gmail.com>
2023-12-25 15:15:23 +0100

    End architecture 🏢 a folder structure

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <h1 className="text-6xl font-medium text-blue-600">Vite + React</h1>
+        </div>
+    );
+};
+
+export default Home;
